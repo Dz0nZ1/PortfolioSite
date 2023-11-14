2f067241fa047c9b3cfae967e1d0f76ef192e74b
Dz0nZ1 <dzonzi777@gmail.com>
2023-11-14 16:22:02 +0100

    Created Links component

---
+++ src/components/Sidebar/Links/Links.jsx
@@ -0,0 +1,56 @@
+import {motion} from "framer-motion";
+
+export default function Links() {
+
+
+    const variants = {
+        open: {
+            transition: {
+                staggerChildren: 0.1
+            }
+        },
+        closed: {
+            transition: {
+                staggerChildren: 0.05,
+                staggerDirection: -1,
+            }
+        }
+    }
+
+
+    const itemVariants = {
+        open: {
+            y: 0,
+            opacity: 1
+        },
+        closed: {
+           y: 50,
+           opacity: 0
+        }
+    }
+
+
+    const items = [
+        "Homepage",
+        "Services",
+        "Portfolio",
+        "Contact",
+        "about"
+    ]
+
+
+    return(
+
+        <motion.div className="links" variants={variants}>{items.map((item, index) => {
+            return(
+                <motion.a
+                    href={`#${item}`}
+                    key={index}
+                    variants={itemVariants}
+                    whileHover={{scale: 1.1}}
+                    whileTap={{scale: 0.95}}
+                >{item}</motion.a>
+            )
+        })}</motion.div>
+    )
+}
