68f5690549d20e17083f982ad72d6e35f72c7ac6
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-11 20:50:55 +0100

    Updated Navbar with open context

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,39 @@
 import "./navbar.scss"
 import {motion} from "framer-motion";
 import Sidebar from "../Sidebar/Sidebar.jsx";
+import {useContext} from "react";
+import OpenContext from "../../context/OpenContext.jsx";
 export default function Navbar() {
 
-    const variants = {
+    const {open} = useContext(OpenContext);
+
+
+    const moveVariants = {
+        initial: {
+            x: 0,
+            opacity: 1,
+        },
+        animate: {
+            x: 200,
+            opacity: 1,
+            transition: {
+                duration: 0.5,
+                staggerChildren: 0.1
+            }
+        }
+    }
+
+
+
+    const textVariants = {
         starter: {opacity : 0, scale: 0.5},
         animate : {opacity: 1, scale: 1},
         duration : {duration: 2}
     }
+
+
+    const variants = open ? moveVariants : textVariants;
+
 
     return(
         <div className="navbar">
@@ -18,12 +44,10 @@ export default function Navbar() {
                 initial="starter"
                 animate="animate"
                 transition="duration"
-                >Nikola</motion.span>
+                >Nikola Lelekovic</motion.span>
                 <div className="social">
-                    <a href="#"><img src="/facebook.png" alt="facebook"/></a>
                     <a href="#"><img src="/instagram.png" alt="instagram"/></a>
-                    <a href="#"><img src="/youtube.png" alt="youtube"/></a>
-                    <a href="#"><img src="/dribbble.png" alt="dribble"/></a>
+                    <a href="#"><img src="/linkedin.png" alt="linkedin"/></a>
                 </div>
             </div>
         </div>
